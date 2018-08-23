b4f494e9c1616ffa608b46e7b216738bd7d5584b
Salinas <john.salinas@accenture.com>
2018-08-23 18:53:52 -0300

    JOHNSANA PRUEBE

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { TableBasicExample } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
